d6613be18951dd8891cbe90e3d3e656e0d853312
ajlopez <webmaster@ajlopez.com>
2015-12-12 09:04:44 -0300

    Improving user tests, adding username

--- test/user.js
+++ test/user.js
@@ -6,7 +6,7 @@ var adamId;
 exports['add user'] = function (test) {
     test.async();
     
-    userService.addUser({ name: 'Adam' }, function (err, id) {
+    userService.addUser({ username: 'adam', name: 'Adam' }, function (err, id) {
         test.ok(!err);
         test.ok(id);
         adamId = id;
@@ -23,6 +23,7 @@ exports['get user by id'] = function (test) {
         test.equal(typeof user, 'object');
         
         test.ok(user.name);
+        test.equal(user.username, 'adam');
         test.equal(user.name, 'Adam');
         test.equal(user.id, adamId);
         
@@ -40,6 +41,7 @@ exports['get users'] = function (test) {
         test.ok(users.length);
         test.equal(users.length, 1);
         
+        test.equal(users[0].username, 'adam');
         test.equal(users[0].name, 'Adam');
         test.equal(users[0].id, adamId);
         
