7a019e605bc83fc9f1e0781c0dbf54dcce59a58b
ajlopez <webmaster@ajlopez.com>
2015-12-12 11:11:36 -0300

    First admin views

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var adminRoutes = require('./routes/admin.js');
 var adminUserRoutes = require('./routes/admin/user');
 
 var app = express();
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-app.use('/user/project', adminUserRoutes);
+app.use('/admin', adminRoutes);
+app.use('/admin/user', adminUserRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
