c9a7e49b926e3343611792eb876deaaa3ce95f18
ajlopez <webmaster@ajlopez.com>
2015-12-12 08:53:37 -0300

    Get user by id, using ostore

--- services/user.js
+++ services/user.js
@@ -1,9 +1,18 @@
 
+var ostore = require('ostore');
+
+var store = ostore.createStore('users');
+
 function addUser(user, cb) {
-    cb(null, 1);
+    cb(null, store.add(user));
 };
 
+function getUserById(id, cb) {
+    cb(null, store.get(id));
+}
+
 module.exports = {
-    addUser: addUser
+    addUser: addUser,
+    getUserById: getUserById
 };
 
--- test/user.js
+++ test/user.js
@@ -1,12 +1,31 @@
 
 var userService = require('../services/user');
 
+var adamId;
+
 exports['add user'] = function (test) {
     test.async();
     
     userService.addUser({ name: 'Adam' }, function (err, id) {
         test.ok(!err);
         test.ok(id);
+        adamId = id;
+        test.done();
+    });
+};
+
+exports['get user by id'] = function (test) {
+    test.async();
+    
+    userService.getUserById(adamId, function (err, user) {
+        test.ok(!err);
+        test.ok(user);
+        test.equal(typeof user, 'object');
+        
+        test.ok(user.name);
+        test.equal(user.name, 'Adam');
+        test.equal(user.id, adamId);
+        
         test.done();
     });
 };
