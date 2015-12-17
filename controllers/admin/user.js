0fd643d3cffb0a691a97517ce9faa5144010f717
ajlopez <webmaster@ajlopez.com>
2015-12-17 09:25:59 -0300

    Loans in user view

--- controllers/admin/user.js
+++ controllers/admin/user.js
@@ -2,6 +2,7 @@
 var async = require('simpleasync');
 
 var userService = require('../../services/user');
+var loanService = require('../../services/loan');
 var translate = require('../../utils/translate');
 
 function listUsers(req, res) {
@@ -21,14 +22,32 @@ function listUsers(req, res) {
 }
 
 function viewUser(req, res) {
-    userService.getUserById(req.params.id, function (err, user) {
-        if (err)
-            return res.render('admin/error', { error: err });
-        
+    var model = { };
+    var id = req.params.id;
+    
+    if (id.length && id.length < 6)
+        id = parseInt(id);
+    
+    async()
+    .then(function (data, next) {
+        userService.getUserById(id, next);
+    })
+    .then(function (user, next) {
         user.scoringDescription = translate.scoring(user.scoring);
-        
-        res.render('admin/userView', { user: user });
-    });
+        model.user = user;
+        loanService.getLoansByUser(id, next);
+    })
+    .then(function (loans, next) {
+        translate.statuses(loans);
+
+        model.loans = loans;
+
+        res.render('admin/userView', model);
+    })
+    .fail(function (err) {
+        res.render('admin/error', { error: err });
+    })
+    .run();
 }
 
 function newUser(req, res) {
