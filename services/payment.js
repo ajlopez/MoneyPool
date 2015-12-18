e2e63c1fea94fb89f34ac0fe97b500c8a9ec485a
ajlopez <webmaster@ajlopez.com>
2015-12-18 08:15:51 -0300

    Payment service using store, mongodb test

--- services/payment.js
+++ services/payment.js
@@ -4,30 +4,38 @@
 var db = require('../utils/db');
 var dates = require('../utils/dates');
 
-var store = db.createStore('payments');
-
 function clearPayments(cb) {
-    store = db.createStore('payments');
+    db.createStore('payments');
     cb(null, null);
 };
 
 function newPayment(payment, cb) {
+    var store = db.store('payments');
+
     store.add(payment, cb);
 };
 
 function getPaymentById(id, cb) {
+    var store = db.store('payments');
+
     store.get(id, cb);
 }
 
 function getPaymentsByLoan(loanId, cb) {
+    var store = db.store('payments');
+
     store.find({ loan: loanId }, cb);
 }
 
 function getPayments(cb) {
+    var store = db.store('payments');
+
     store.find(cb);
 }
 
 function updatePayment(id, data, cb) {
+    var store = db.store('payments');
+
     store.update(id, data, cb);
 }
 
